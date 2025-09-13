569d2a88a10441d83e62b4f543f95d477ae71eef
AshutoshSharma123 <aashusharma375@gmail.com>
2025-09-13 13:34:47 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar.jsx'
 const App = () => {
   return (
     <div className='font-[font1] w-full min-h-screen  text-black bg-cover bg-repeat bg-center bg-style p-5 '
-      style={{ backgroundImage: "url(/src/assets/wallpaper.jpg)" }}>
+      style={{ backgroundImage: `url(${import.meta.env.BASE_URL}images/wallpaper.jpg)` }}>
       <Navbar />
       <MainRoutes />
 
