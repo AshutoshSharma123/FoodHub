0bd3924285516a0b105df0ce7ee27f714dada809
AshutoshSharma123 <aashusharma375@gmail.com>
2025-09-27 15:48:56 +0530

    added images correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,22 @@
-import React from 'react';
+// import React from 'react';
+// import MainRoutes from './routes/MainRoutes.jsx';
+// import Navbar from './components/Navbar.jsx';
+
+// const App = () => {
+//   return (
+//     <div
+//       className="font-[font1] w-full min-h-screen text-black bg-cover bg-center p-5"
+//       style={{ backgroundImage: "url('/images/wallpaper.jpg')" }}
+//     >
+//       <Navbar />
+//       <MainRoutes />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
 import MainRoutes from './routes/MainRoutes.jsx';
 import Navbar from './components/Navbar.jsx';
 
@@ -6,7 +24,7 @@ const App = () => {
   return (
     <div
       className="font-[font1] w-full min-h-screen text-black bg-cover bg-center p-5"
-      style={{ backgroundImage: "url('/images/wallpaper.jpg')" }}
+      style={{ backgroundImage: "url('FoodHub/images/wallpaper.jpg')" }}
     >
       <Navbar />
       <MainRoutes />
