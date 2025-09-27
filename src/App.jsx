4db2bbf39a38a9003a4e4e86fcb1cd596a324f3e
AshutoshSharma123 <aashusharma375@gmail.com>
2025-09-27 15:27:32 +0530

    set up fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import MainRoutes from './routes/MainRoutes.jsx'
-import Navbar from './components/Navbar.jsx'
-import logo from '../public/images/wallpaper.jpg';
+import React from 'react';
+import MainRoutes from './routes/MainRoutes.jsx';
+import Navbar from './components/Navbar.jsx';
+
 const App = () => {
   return (
     <div
-      className='font-[font1] w-full min-h-screen text-black bg-cover bg-center p-5'
+      className="font-[font1] w-full min-h-screen text-black bg-cover bg-center p-5"
       style={{ backgroundImage: "url('/images/wallpaper.jpg')" }}
     >
       <Navbar />
       <MainRoutes />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
