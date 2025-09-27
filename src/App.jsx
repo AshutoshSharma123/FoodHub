679e80ae06126186389152a2ab4b8b0f293c86d6
AshutoshSharma123 <aashusharma375@gmail.com>
2025-09-27 14:02:08 +0530

    Fix background image and font loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import MainRoutes from './routes/MainRoutes.jsx'
 import Navbar from './components/Navbar.jsx'
+import logo from '../public/images/wallpaper.jpg';
 const App = () => {
   return (
     <div className='font-[font1] w-full min-h-screen  text-black bg-cover bg-repeat bg-center bg-style p-5 '
-      style={{ backgroundImage: `url(${import.meta.env.BASE_URL}images/wallpaper.jpg)` }}>
+      style={{ backgroundImage: `url(${logo})` }}>
       <Navbar />
       <MainRoutes />
 
